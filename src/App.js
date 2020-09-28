7d2e82ebc85f45157a41e1d02e5658ec55e1dd50
Rupinderthind <rupinderpalthind@gmail.com>
2020-09-28 17:38:23 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Login from './component/login';
+import Dashboard from './component/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+      <ul>
+        <li>
+          <Link to={'/'}>Login </Link>
+        </li>
+        <li>
+          <Link to={'/home'}>Home </Link>
+        </li>
+        <li>
+          <Link to={'/about'}>About us </Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/home' component={Dashboard} />
+        <Route exact path='/about' component={Login} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
